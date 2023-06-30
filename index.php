a7b671a488ec94f362798b8674c432502140f00c
Robert McQueen <robertmcqueen@roberts-mini.bc.edu>
2023-06-30 15:07:14 -0400

    mvp of homepage

--- index.php
+++ index.php
@@ -37,9 +37,56 @@ queue_js_string('
     <?php endif; ?>
 </div><!--end featured-item-->
 
-<?php if (get_theme_option('Homepage Text')): ?>
-<div id="homepage-text"><?php echo get_theme_option('Homepage Text'); ?></div>
-<?php endif; ?>
+<div id="homepage-text-block">
+  <div id="learn-more">
+    <h1>FWWCP</h1>
+    <h2>Digital Collections</h2>
+    <a href="/about"><button>Learn more</button></a>
+  </div>  
+  <?php if (get_theme_option('Homepage Text')): ?>
+  <div id="homepage-text"><?php echo get_theme_option('Homepage Text'); ?></div>
+  <?php endif; ?>
+  </div>
+</div>
+
+<div id="getting-started-block">
+  <h2>GETTING STARTED</h2>
+  <div id="getting-started-options">
+    <div class="getting-started-option">
+      <a href="/using-collections">
+        <img src="http://fwwcpdigitalcollection.org/files/original/8717104c1b72f71fc8e324ab101f3ed3.png" width="300px" />
+      </a>  
+      <h3>Using the Collections</h3>
+    </div>
+    <div class="getting-started-option">
+      <a href="/reading-guides">
+        <img src="http://fwwcpdigitalcollection.org/files/original/0ae7c530967cc9078390cac6fdb4dcc8.png" width="300px" />
+      </a>  
+      <h3>Reading Guides</h3>
+    </div>
+    <div class="getting-started-option">
+      <a href="/history">
+        <img src="http://fwwcpdigitalcollection.org/files/original/4248af7c7e7e109b6a10c8a25b1425f5.jpg" width="300px" />
+      </a>
+      <h3>FWWCP History</h3>
+    </div>        
+  </div>
+</div>  
+
+<div id="why-use-these-collections-block">
+  <h2>WHY USE THESE COLLECTIONS</h2>
+  <div id="testimonial-container">
+    <div class="testimonial">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/1qQ0XUz26yY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    <div class="testimonial">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/1qQ0XUz26yY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+  </div>
+  <div id="watch-more-container">
+    <a href="why-use-collections"><button>Watch More</button></a>
+  </div>
+</div>  
 
 <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
 
